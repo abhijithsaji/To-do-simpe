9efde8e02577a2d0052d0eea9328bfa4f8abc87e
abhijithsaji <007myofficial@gmail.com>
2020-04-28 22:10:25 +0530

    deploying req

--- to_do/settings.py
+++ to_do/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e-tk_v@y&c&6#b$(&$9=*^r%eq+tu=bpt^2q=@6z&ny^qn5v)a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['abhi-testings.herokuapp.com','127.0.0.1']
 
 
 # Application definition
